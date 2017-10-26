d47401b35d6bf0d33deb010a97a1243502937c0f
JessicaFresnedaH <Jessyinad@gmail.com>
2017-10-26 11:25:11 -0500

    implementacion JuegoNuevo

--- src/main/java/edu/eci/arsw/aplicacion/Partida.java
+++ src/main/java/edu/eci/arsw/aplicacion/Partida.java
@@ -10,5 +10,9 @@ package edu.eci.arsw.aplicacion;
  * @author 2089340
  */
 public class Partida {
+
+    public Partida(String nombre, String TipPartida, String idPartida, int filas, int columnas, String jugador, String nivel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
