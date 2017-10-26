5241260337b9d476451e2c4941e637c392334f47
JessicaFresnedaH <Jessyinad@gmail.com>
2017-10-26 09:03:21 -0500

    jugador implementado

--- src/main/java/edu/eci/arsw/aplicacion/Jugador.java
+++ src/main/java/edu/eci/arsw/aplicacion/Jugador.java
@@ -11,4 +11,51 @@ package edu.eci.arsw.aplicacion;
  */
 public class Jugador {
     
+    private int numVidas;
+    private String nombre;
+    private String color;
+    private boolean  estadoVivo=true;
+    
+    public Jugador(int numVidas, String nombre, String color) {
+        this.numVidas = numVidas;
+        this.nombre = nombre;
+        this.color = color;
+        this.estadoVivo = estadoVivo;
+    }
+
+    public int getNumVidas() {
+        return numVidas;
+    }
+
+    public void setNumVidas(int numVidas) {
+        this.numVidas = numVidas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isEstadoVivo() {
+        return estadoVivo;
+    }
+
+    public void setEstadoVivo(boolean estadoVivo) {
+        this.estadoVivo = estadoVivo;
+    }
+
+    
+    
+    
 }
