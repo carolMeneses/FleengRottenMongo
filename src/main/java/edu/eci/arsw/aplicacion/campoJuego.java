0f06159d88af9bf002684ca6b9df5bac1e83cfac
carolMeneses <carol.meneses@mail.escuelaing.edu.co>
2017-10-26 11:05:38 -0500

    Creando campo de juego para varios juegos online

---
+++ src/main/java/edu/eci/arsw/aplicacion/campoJuego.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.aplicacion;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 2089340
+ */
+public class campoJuego {
+    private ArrayList<Juego> juegos= new ArrayList<Juego>();
+    
+    // Se agregan los datos que el usuario a ingresado por medio del HTML-controllador, clase JUego Nuevo
+    public boolean CrearPartida(JuegoNuevo jn){
+    Juego j=jn.getNuevoJuego();
+    j.JuegoNuevo();
+    j.agregarJugador(jn.getJugador());
+    boolean crear=juegos.add(j);
+    return crear;
+    }
+    
+    
+}
