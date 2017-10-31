45f78cd3164686cf0fc54ed10473bb5d8b76001a
carol.meneses <carol.meneses@mail.escuelaing.edu.co>
2017-10-30 21:25:26 -0500

    Service y persistencia

--- src/main/java/edu/eci/arsw/services/apiServices.java
+++ src/main/java/edu/eci/arsw/services/apiServices.java
@@ -5,6 +5,12 @@
  */
 package edu.eci.arsw.services;
 
+import edu.eci.arsw.model.Jugador;
+import edu.eci.arsw.model.Partida;
+import edu.eci.arsw.persistencia.applePersistence;
+import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,7 +19,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class apiServices {
-    //@Autowired
+   @Autowired
+   applePersistence apple;
     
-    
+ 
+    public boolean agregarJugador(Partida pn,String nombre) {
+     return apple.agregarJugador(pn, nombre);
+    }
+
+   
+    public List<Jugador> getJugadores(Partida pn) {
+           return apple.getJugadores(pn);
+    }
+  
+    public Partida getPartidaJugador(Jugador j) {
+        return apple.getPartidaJugador(j);
+    }
+    public Set<Partida> getPartidasByTipo(String tipoPartida) {
+        return apple.getPartidasByTipo(tipoPartida);
+    }
+
+    public void eliminar(Partida p) {
+         apple.eliminar(p);
+    }
+
+    public void crearNuevoPartida(Partida p) {
+     apple.crearNuevoPartida(p);
+    }
 }
